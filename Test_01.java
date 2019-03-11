ab42fef581b93f1daf53e5178dbcc702c29cc754
jogeshparida <47854918+jogeshparida@users.noreply.github.com>
2019-03-11 17:50:37 +0530

    Update Test_01.java

--- Test_01.java
+++ Test_01.java
@@ -1,7 +1,6 @@
 public class Test_01{
 
      public static void main(String []args){
-        System.out.println("Hello World");  
-          
+        System.out.println("Hello World");       
      }
 }
